702f254e19a9d8db4781fe47fe2a19b2f2551c1d
Joseph <1josephsimard@gmail.com>
2026-04-16 09:19:16 -0400

    data de notre beau spectro de la mort

---
+++ projet2/graph.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
+
+pixels = []
+Intensite = []
+
+# Fichier BMP
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = np.array(Image.open("laser_PINHOLE_532n.bmp"))
+
+profil = np.mean(img, axis=0)
+
+plt.plot(profil)
+plt.title("Profil d'intensité")
+plt.show()
